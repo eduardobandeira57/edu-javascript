4df7180dd8671a17ab59c1661fba041d06e3e1b8
eduardobandeira57 <eduapp196@gmail.com>
2025-08-10 08:51:38 -0300

    Update indexaula1modules.js

--- indexaula1modules.js
+++ indexaula1modules.js
@@ -4,9 +4,10 @@ import {hoje} from './hoje.js'
 var nome = document.querySelector(".nome")
 var dat = document.querySelector(".dat")
 
-
+dat.innerHTML=hoje
     nome.innerHTML=nomes[1]
-    dat.innerHTML=hoje
+    
+
 
 
 
